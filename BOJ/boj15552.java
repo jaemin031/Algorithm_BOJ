306344fc8618623f3545d85821e6f5a657dbba64
jaemin031 <34496139+jaemin031@users.noreply.github.com>
2020-04-07 18:47:07 +0900

    [JAVA][백준 15552] 빠른 A+B

---
+++ BOJ/boj15552.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		try {
+			String line = br.readLine();
+			for(int i=0;i<Integer.parseInt(line);i++) {
+				String[] aa = br.readLine().split(" ");
+				bw.write(Integer.parseInt(aa[0]) + Integer.parseInt(aa[1])+"\n");
+				
+			}
+			bw.flush();
+			bw.close();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
