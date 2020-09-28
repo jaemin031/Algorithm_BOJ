231ef903ca6eda592890e4b5f912354208f914e9
jaemin031 <34496139+jaemin031@users.noreply.github.com>
2020-09-28 23:13:32 +0900

    [JAVA][백준 1929] 소수 구하기
    #정수론 #소수 판정 #에라토스테네스의 체

---
+++ BOJ/boj1929.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       
+        
+        try {
+        	 StringTokenizer st = new StringTokenizer(br.readLine());
+        	 int M = Integer.parseInt(st.nextToken());
+        	 int N = Integer.parseInt(st.nextToken());
+        	 
+        	 for(int i = M; i<=N;i++) {
+        		 
+        		 if(isPrime(i)&&i!=1)
+        			 bw.write(i+ "\n");
+        		 
+        	 }
+        	
+        	
+        	
+			bw.flush();
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean isPrime(int n) {
+			boolean flag = true;
+			int end = (int)Math.sqrt(n);
+	
+			for(int j=2; j<=end; j++) {
+				if(n%j==0) {
+					flag = false;
+					break;
+				}
+			}
+			
+			
+			return flag;
+	}
+}
